7e96b423acf0a174c3e680fe86543d320ce6c2cb
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2017-10-07 19:55:35 +0100

    added icon and background

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 import $ from 'jquery';
 const css = require('./style.css');
 
-
 const keyboardControls = () => {
   $(document).keydown((e) => {
-    if(e.originalEvent.code === 'ArrowLeft' ){
-        $('#character').animate({
-            left: '-=10px',
-            duration: 0.1
-        }, () => {
-            //animation complete
-        });
+    if(e.originalEvent.code === 'ArrowLeft' ) {
+      $('#character').animate({
+        left: '-=10px',
+        duration: 0.1
+      }, 0, 'linear');
     }
 
-    if(e.originalEvent.code === 'ArrowRight' ){
-        $('#character').animate({
-            left: '+=10px',
-            duration: 0.1
-        }, () => {
-            //animation complete
-        });
+    if(e.originalEvent.code === 'ArrowRight' ) {
+      $('#character').animate({
+        left: '+=10px',
+        duration: 0.1
+      }, 0, 'linear');
     }
+
+    if(e.originalEvent.code === 'ArrowDown' ) {
+      $('#character').animate({
+        top: '+=10px',
+        duration: 0.1
+      }, 0, 'linear');
+    }
+
+    if(e.originalEvent.code === 'ArrowUp' ) {
+      $('#character').animate({
+        top: '-=10px',
+        duration: 0.1
+      }, 0, 'linear');
+    }
+
+
   })
 };
 
+// assign my key movement to
 $(() => {
   console.log('Starting programe');
   keyboardControls();
