011817f92b5d1e054e8d47902469497f0b7d4cf2
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2017-10-09 14:57:28 +0100

    game logic added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import $ from 'jquery';
 const css = require('./style.css');
+const menu = $('.title_screen');
+// const $collisionBox = $(box);
+// const $collisionBoxPosition = $collisionBox.position();
+// const $collisionBoxWidth = $collisionBox.width();
+// const $collisionBoxheight = $collisionBox.height();
 
 const keyboardControls = () => {
+  const position =$('#character').position();
   $(document).keydown((e) => {
     if(e.originalEvent.code === 'ArrowLeft' ) {
       $('#character').animate({
@@ -9,12 +15,13 @@ const keyboardControls = () => {
         duration: 0.1
       }, 0, 'linear');
     }
-
     if(e.originalEvent.code === 'ArrowRight' ) {
-      $('#character').animate({
-        left: '+=10px',
-        duration: 0.1
-      }, 0, 'linear');
+      // if (position.right < 200){
+        $('#character').animate({
+          left: '+=10px',
+          duration: 0.1
+        }, 0, 'linear');
+      // }
     }
 
     if(e.originalEvent.code === 'ArrowDown' ) {
@@ -30,14 +37,31 @@ const keyboardControls = () => {
         duration: 0.1
       }, 0, 'linear');
     }
-
-
-  })
+  });
 };
 
 // assign my key movement to
 $(() => {
   console.log('Starting programe');
   keyboardControls();
-  const existingdiv1 = document.getElementById('character');
+  createCloud();
+  setInterval(moveCloud,500);
 });
+
+function createCloud() {
+
+  //identify the div that we want to append children to
+  const $sky = $('.sky');
+
+  //append a new div for the cloud to $sky
+  $sky.append('<div class="cloud"></div>');
+  console.log($sky);
+  //animate function to move the cloud from right to left
+}
+function moveCloud(){
+
+  // animate(properties[, duration][, easing][, callback])
+  $('.cloud').animate({'right': '-=50', duration: 0.1 },10000,'linear');
+
+
+}
